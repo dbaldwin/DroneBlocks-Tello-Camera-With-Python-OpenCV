e064c196936077dee7a253536e3ea96a04a2756f
Dennis Baldwin <dennisbaldwin@gmail.com>
2019-10-27 13:56:23 -0500

    Resizing and testing on Win

--- camera.py
+++ camera.py
@@ -1,13 +1,18 @@
 import cv2
 
+# Size down the default frame size from 960x720 to 480x360
+frame_size = (480, 360)
+
 class VideoCamera(object):
     def __init__(self):
-        self.aruco = Aruco()
+        #self.aruco = Aruco()
         self.cap = cv2.VideoCapture('udp://127.0.0.1:11111')
-        #self.frame = cv2.resize(frame, (480, 360))
+        _, frame = self.cap.read()
+        self.frame = cv2.resize(frame, frame_size)
 
     def get_frame(self):
         _, frame = self.cap.read()
+        frame = cv2.resize(frame, frame_size)
 
         # The first detected frame is None
         if frame is None:

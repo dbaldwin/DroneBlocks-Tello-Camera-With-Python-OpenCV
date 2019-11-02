bb67830a0d5ee4f8e95ca2da31ac30388aac4356
Dennis Baldwin <dennisbaldwin@gmail.com>
2019-11-02 14:44:29 -0500

    Left stick commands

--- static/js/keyboard.js
+++ static/js/keyboard.js
@@ -1,19 +1,36 @@
 $(document).keydown(function(e) {
+
     switch(e.which) {
-        case 37: // left
+        case 37: // left arrow for fly left
         post("/send_command", JSON.stringify({"command": "left 20"}));
         break;
 
-        case 38: // up
+        case 38: // up arrow for fly forward
         post("/send_command", JSON.stringify({"command": "forward 20"}));
         break;
 
-        case 39: // right
+        case 39: // right arrow for fly right
         post("/send_command", JSON.stringify({"command": "right 20"}));
         break;
 
-        case 40: // down
+        case 40: // down arrow for fly backward
         post("/send_command", JSON.stringify({"command": "back 20"}));
+        break;
+
+        case 87: // W key for fly up
+        post("/send_command", JSON.stringify({"command": "up 20"}));
+        break;
+
+        case 65: // A key for yaw left
+        post("/send_command", JSON.stringify({"command": "ccw 90"}));
+        break;
+
+        case 83: // S key for fly down
+        post("/send_command", JSON.stringify({"command": "down 20"}));
+        break;
+
+        case 68: // D key for yaw right
+        post("/send_command", JSON.stringify({"command": "cw 90"}));
         break;
 
         default: return;
